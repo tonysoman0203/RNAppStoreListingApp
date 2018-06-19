0cd1d6dc241d6ad2e1360f140d0d6c520d2229ad
Tony So <tony.so@gtomato.com>
2018-06-19 17:02:11 +0800

    *Update* rename and add function for filter and text is empty

--- src/js/utils/Utils.js
+++
@@ -1,9 +0,0 @@
-export default class Utils {
-    static buildMap(obj) {
-        let map = new Map();
-        Object.keys(obj).forEach(key => {
-            map.set(key, obj[key]);
-        });
-        return map;
-    }
-}
---
+++ src/js/utils/index.js
@@ -0,0 +1,30 @@
+import {Dimensions } from 'react-native'
+export default class Utils {
+    static buildMap(obj) {
+        let map = new Map();
+        Object.keys(obj).forEach(key => {
+            map.set(key, obj[key]);
+        });
+        return map;
+    }
+    static isLandscape = () => {
+        const dim = Dimensions.get('window');
+        return dim.width >= dim.height;
+    };
+
+    static isEmpty = (text) => {
+        return text == '' || text == null || text == undefined
+    }
+
+    static checkAppContainsKey(source: Array, key){
+        return source.filter((item, index, array)=>{
+                var map = this.buildMap(item.entry)
+                var pName = map.get(`im:name`).label
+                var pSummary = map.get(`summary`).label
+                var pcategory = map.get(`category`).attributes.term
+                var pAuthor = map.get(`im:artist`).label
+                // console.log(`productMap find pName = ${pName} pcategory:${pcategory} pAuthor: ${pAuthor} pSummary=${pSummary}`);
+                return pName.includes(key) || pSummary.includes(key) || pcategory.includes(key) || pAuthor.includes(key)
+        })
+    }
+}
